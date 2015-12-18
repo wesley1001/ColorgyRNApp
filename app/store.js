0f8b4fcd3a3fa7eac4bdad0c9499decf7a57ed53
Neson <neson@dex.tw>
2015-12-18 16:09:06 +0800

    persist store

--- app/store.js
+++ app/store.js
@@ -1,4 +1,6 @@
 import { createStore, applyMiddleware } from 'redux';
+import { persistStore, autoRehydrate } from 'redux-persist';
+import { AsyncStorage } from 'react-native';
 import reducers from './reducers';
 import thunkMiddleware from 'redux-thunk';
 import createLogger from 'redux-logger';
@@ -14,6 +16,11 @@ const createStoreWithMiddleware = applyMiddleware(
   loggerMiddleware
 )(createStore);
 
-let store = createStoreWithMiddleware(reducers);
+let store = autoRehydrate()(createStoreWithMiddleware)(reducers);
+
+persistStore(store, {
+  storage: AsyncStorage,
+  whitelist: ['counter']
+});
 
 export default store;

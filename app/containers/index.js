b902613269fd757995e5acb11f9a29338cf0997a
Neson <neson@dex.tw>
2015-12-18 16:12:20 +0800

    provide SQL database

--- app/containers/index.js
+++ app/containers/index.js
@@ -2,13 +2,18 @@ import React, {
   StyleSheet,
   Text,
   View,
+  ScrollView,
   TouchableOpacity
 } from 'react-native';
 import { connect } from 'react-redux/native';
 
+import courseDatabase, { sqlValue } from '../databases/courseDatabase';
 import { counterPlus, asyncCounterPlus } from '../actions/counterActions';
 
 var AppContainer = React.createClass({
+  getInitialState: function() {
+    return {};
+  },
 
   _handleCount: function() {
     this.props.dispatch(counterPlus());
@@ -18,34 +23,119 @@ var AppContainer = React.createClass({
     this.props.dispatch(asyncCounterPlus());
   },
 
+  _handleDatabaseInsert: function() {
+    var data = {
+      code: Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 20),
+      general_code: Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 20),
+      name: Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 20)
+    };
+
+    courseDatabase.executeSql(`INSERT INTO courses (code, general_code, name) VALUES (${sqlValue(data.code)}, ${sqlValue(data.general_code)}, ${sqlValue(data.name)});`).then((r) => {
+
+      this.setState({ dbResults: JSON.stringify(data) });
+    }).catch((e) => {
+      this.setState({ dbResults: JSON.stringify(e) });
+    });
+  },
+
+  _handleDatabaseSelectAll: function() {
+    courseDatabase.executeSql('SELECT * FROM courses;').then((r) => {
+      var data = {};
+
+      if (r.results.rows.length) {
+        for (let i=0; i<r.results.rows.length; i++) {
+          let row = r.results.rows.item(i);
+          data[row.code] = row;
+        }
+      }
+
+      this.setState({ dbResults: JSON.stringify(data) });
+    }).catch((e) => {
+      this.setState({ dbResults: JSON.stringify(e) });
+    });
+  },
+
+  _handleDatabaseDeleteAll: function() {
+    courseDatabase.executeSql('DELETE FROM courses;').then((r) => {
+      this.setState({ dbResults: 'All Deleted!' });
+    }).catch((e) => {
+      this.setState({ dbResults: JSON.stringify(e) });
+    });
+  },
+
+  _handleDatabaseReset: function() {
+    courseDatabase.reset().then(() => {
+      courseDatabase.migrate();
+      this.setState({ dbResults: 'DB has been reset!' });
+    }).catch((e) => {
+      this.setState({ dbResults: JSON.stringify(e) });
+    });
+  },
+
   render: function() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          This is a simple counter. Press the button below to count.
-        </Text>
-        <Text style={styles.instructions}>
-          {this.props.count}
-        </Text>
-        <TouchableOpacity onPress={this._handleCount}>
-          <Text style={styles.action}>
-            Count!
+        <ScrollView contentContainerStyle={styles.scrollView}>
+          <Text style={styles.welcome}>
+            Welcome to React Native!
           </Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={this._handleAsyncCount}>
-          <Text style={styles.action}>
-            Async Count!
+          <Text style={styles.instructions}>
+            This is a simple counter. Press the button below to count.
           </Text>
-        </TouchableOpacity>
+          <Text style={styles.instructions}>
+            {this.props.count}
+          </Text>
+          <TouchableOpacity onPress={this._handleCount}>
+            <Text style={styles.action}>
+              Count!
+            </Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={this._handleAsyncCount}>
+            <Text style={styles.action}>
+              Async Count!
+            </Text>
+          </TouchableOpacity>
+          <Text style={styles.instructions}>
+            - - -
+          </Text>
+          <Text style={styles.instructions}>
+            And below is a database demo.
+          </Text>
+          <Text>
+            {this.state.dbResults}
+          </Text>
+          <TouchableOpacity onPress={this._handleDatabaseInsert}>
+            <Text style={styles.action}>
+              Insert!
+            </Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={this._handleDatabaseSelectAll}>
+            <Text style={styles.action}>
+              Select All!
+            </Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={this._handleDatabaseDeleteAll}>
+            <Text style={styles.action}>
+              Delete All!
+            </Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={this._handleDatabaseReset}>
+            <Text style={styles.action}>
+              Reset DB!
+            </Text>
+          </TouchableOpacity>
+        </ScrollView>
       </View>
     );
   }
 });
 
 var styles = StyleSheet.create({
+  scrollView: {
+    padding: 16,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
   container: {
     flex: 1,
     justifyContent: 'center',
@@ -53,7 +143,7 @@ var styles = StyleSheet.create({
     backgroundColor: '#F5FCFF',
   },
   welcome: {
-    fontSize: 20,
+    fontSize: 32,
     textAlign: 'center',
     margin: 10,
   },
@@ -63,7 +153,7 @@ var styles = StyleSheet.create({
     marginBottom: 5,
   },
   action: {
-    fontSize: 16,
+    fontSize: 18,
     textAlign: 'center',
     color: '#333333',
     marginBottom: 5,
---
+++ app/utils/WebSQL.js
@@ -0,0 +1,205 @@
+// usage example:
+//
+//  var migartions = {
+//    '1.0': "CREATE TABLE todos(ID INTEGER PRIMARY KEY, name TEXT);",
+//    '1.1': "ALTER TABLE todos ADD done BOOLEAN NOT NULL DEFAULT FALSE;",
+//    '2.0': "CREATE TABLE users(ID INTEGER PRIMARY KEY, name TEXT);",
+//    '2.1': "ALTER TABLE todos ADD user_id INTEGER;",
+//  }
+//  var db = new WebSQL('TestDB', 'TestDB', 5*1024*1024, migartions);
+//
+//  db.migrate();
+//
+//  db.executeSql('INSERT INTO users (id, name) VALUES (18, "bar")')
+//    .then(function (result) { return result.db.executeSql('INSERT INTO todos (user_id, name) VALUES (18, "task one")') })
+//    .then(function (result) { return result.db.executeSql('INSERT INTO todos (user_id, name) VALUES (18, "task two")') });
+//
+//  db.executeSql('SELECT * FROM users LEFT OUTER JOIN todos ON todos.user_id = users.id WHERE users.id = 12').then(function (result) { console.log(result.results); });
+//
+//  db.executeSql('SELECT * FROM users LEFT OUTER JOIN todos ON todos.user_id = users.id WHERE users.id = ?', [18]).then(function (result) { console.log(result.results); });
+
+class WebSQL {
+  constructor(name, displayName, size, migrations, provider = window, customConstructObject = null) {
+    if (customConstructObject) {
+      this.db = provider.openDatabase(customConstructObject);
+    } else {
+      this.db = provider.openDatabase(name, '', displayName, size || 1024 * 1024);
+    }
+    this.dbName = name;
+    this.migrations = migrations;
+    let migrationKeys = Object.keys(migrations);
+    this.migrationsLastVerison = migrationKeys[migrationKeys.length - 1];
+
+    this.executeSql("CREATE TABLE IF NOT EXISTS _db_info_(key CHAR(255) PRIMARY KEY, value TEXT)", null, false).then( (result) => {
+      this.executeSql("SELECT * FROM _db_info_ WHERE key = 'schema_verison'", null, false).then( (result) => {
+        if (result.results.rows.length && result.results.rows.item(0)) {
+          this.version = result.results.rows.item(0).value;
+        }
+      });
+    });
+  }
+
+  migrate() {
+    var migrateDB = (currentVersion) => {
+
+      var fromVersionIndex = null;
+      var versions = Object.keys(this.migrations);
+      var latestVersion = versions[versions.length - 1];
+
+      if (currentVersion !== latestVersion) {
+
+        if (!currentVersion || currentVersion === '' || currentVersion === 'null') {
+          fromVersionIndex = -1;
+        } else {
+          fromVersionIndex = versions.indexOf(currentVersion);
+
+          if (fromVersionIndex === -1) {
+            console.error(`WebSQL: migration error: Can't locate current version: ${currentVersion} in migrations stack: versions: [${versions.join(', ')}]`);
+            return;
+          }
+        }
+
+        var executeMigrationVersions = versions.slice(fromVersionIndex + 1);
+
+        return executeMigrationVersions.reduce( (previousPromise, version) => {
+          console.log(`WebSQL: database migration required: ${this.dbName}: to ${version}`);
+          return previousPromise.then( () => {
+            return new Promise( (resolve, reject) => {
+              var lastVersion = versions[versions.indexOf(version) - 1];
+              if (!lastVersion) lastVersion = currentVersion;
+              console.log(`WebSQL: migrating database ${this.dbName} from ${lastVersion} to ${version}`);
+              this.db.transaction( (transaction) => {
+                transaction.executeSql(this.migrations[version], null, () => {
+                  transaction.executeSql("INSERT OR REPLACE INTO _db_info_ (key, value) VALUES ('schema_verison' , ?)", [version], () => {
+                    this.version = version;
+                    resolve(this);
+                  }, (t, e) => {
+                    console.error(e);
+                    reject(e);
+                  });
+                }, (t, e) => {
+                  console.error(e);
+                  reject(e);
+                });
+              });
+            });
+          });
+        }, Promise.resolve(this));
+      } else {
+        return Promise.resolve(this);
+      }
+    }
+
+    return this.executeSql("CREATE TABLE IF NOT EXISTS _db_info_(key CHAR(255) PRIMARY KEY, value TEXT)", null, false).then( (result) => {
+
+      return this.executeSql("SELECT * FROM _db_info_ WHERE key = 'schema_verison'", null, false).then( (result) => {
+        var schemaVerison = null;
+        if (result.results.rows.length && result.results.rows.item(0)) {
+          schemaVerison = result.results.rows.item(0).value;
+        }
+
+        return migrateDB(schemaVerison);
+      });
+    });
+  }
+
+  executeSql(sqlQuery, params = [], ensureMigrated = true) {
+    return new Promise( (resolve, reject) => {
+      if (ensureMigrated) var startedAt = (new Date());
+
+      var execute = () => {
+        if (ensureMigrated) {
+          if (this.version != this.migrationsLastVerison) {
+            console.log(`WebSQL: ${this.dbName}: executeSql: Watitig for migration done...`)
+            if ((new Date()) - startedAt > 12000) {
+              reject({ error: 'Migration Timeout' })
+            } else {
+              setTimeout(execute, 100)
+            }
+            return;
+          }
+        }
+
+        this.db.transaction( (t) => t.executeSql(sqlQuery, params, (transaction, results) => {
+          resolve({ db: this, transaction: transaction, results: results });
+        }, (transaction, error) => {
+          reject({ db: this, transaction: transaction, error: error, query: sqlQuery });
+        }) );
+      }
+
+      execute();
+    });
+  }
+
+  reset() {
+    return new Promise( (resolve, reject) => {
+      this.executeSql("SELECT 'DROP TABLE ' || name || ';' FROM sqlite_master WHERE type = 'table';")
+        .then( (result) => {
+          var rows = result.results.rows;
+
+          var dropTablesQueries = [];
+          for (let i=0; i< rows.length; i++) {
+            let row = rows.item(i);
+            let keys = Object.keys(row);
+            dropTablesQueries.push(row[keys[0]]);
+          }
+
+          dropTablesQueries = dropTablesQueries.filter(function (s) { return !s.match(/DROP TABLE __/) });
+
+          let dropTablesQuery = dropTablesQueries.join(' ');
+
+          if (dropTablesQueries && dropTablesQueries.length) {
+
+            let dropTablePromises = [];
+
+            dropTablesQueries.forEach( (dropTablesQuery) => {
+              let p = new Promise( (resolve, reject) => {
+                console.log(`WebSQL: reset: executing ${dropTablesQuery}...`);
+                this.db.transaction( (transaction) => {
+                  transaction.executeSql(dropTablesQuery, null, () => {
+                    resolve();
+                  }, (e) => {
+                    console.error(e);
+                    reject(e);
+                  });
+                });
+              });
+              dropTablePromises.push(p);
+            });
+
+            Promise.all(dropTablePromises).then( () => {
+              this.version = null;
+              console.log(`WebSQL: reset: done.`);
+            }).catch( (e) => {
+              console.error(e);
+              reject(e);
+            });
+          } else {
+            this.version = null;
+            console.log(`WebSQL: reset: done.`);
+          }
+        }).catch( (e) => {
+          console.error(e);
+          reject(e);
+        });
+    });
+  }
+
+  sqlValue(value) {
+    if (value === true) {
+      return 1;
+    } else if (value === false) {
+      return 0;
+    } else if (typeof value === "string") {
+      return "'" + value.replace(/'/g, "''") + "'";
+    } else if (typeof value === "number") {
+      return value;
+    } else {
+      return 'NULL';
+    }
+  }
+}
+
+if (window) window.WebSQL = WebSQL;
+
+if (module) module.exports = WebSQL;

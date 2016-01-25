272358b960f6158fca4c4736129ca91b70f747cc
Neson <neson@dex.tw>
2016-01-25 16:44:47 +0800

    upgrade React Native to 0.18

--- android/app/src/main/java/io/colorgy/rnapp/MainActivity.java
+++ android/app/src/main/java/io/colorgy/rnapp/MainActivity.java
@@ -1,16 +1,11 @@
 package io.colorgy.rnapp;
 
-import android.app.Activity;
-import android.os.Bundle;
-import android.view.KeyEvent;
 import android.content.Intent;
+import android.os.Bundle;
 
-import com.facebook.react.LifecycleState;
-import com.facebook.react.ReactInstanceManager;
-import com.facebook.react.ReactRootView;
-import com.facebook.react.modules.core.DefaultHardwareBackBtnHandler;
+import com.facebook.react.ReactActivity;
+import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
-import com.facebook.soloader.SoLoader;
 
 import com.learnium.RNDeviceInfo.*;
 import org.pgsqlite.SQLitePluginPackage;
@@ -24,91 +19,48 @@ import com.reactnativeandroiddesignsupport.DesignSupportPackage;
 import me.neo.react.StatusBarPackage;
 import com.heng.wheel.WheelPackage;
 
-public class MainActivity extends Activity implements DefaultHardwareBackBtnHandler {
+import java.util.Arrays;
+import java.util.List;
 
-    private ReactInstanceManager mReactInstanceManager;
-    private ReactRootView mReactRootView;
+public class MainActivity extends ReactActivity {
 
-    private FacebookLoginPackage mFacebookLoginPackage;
-
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        mReactRootView = new ReactRootView(this);
-
-        mFacebookLoginPackage = new FacebookLoginPackage(this);
-
-        mReactInstanceManager = ReactInstanceManager.builder()
-                .setApplication(getApplication())
-                .setBundleAssetName("index.android.bundle")
-                .setJSMainModuleName("index.android")
-                .addPackage(new MainReactPackage())
-                .addPackage(new RNDeviceInfo())
-                .addPackage(new SQLitePluginPackage(this))
-                .addPackage(new GoogleAnalyticsPackage(this))
-                .addPackage(mFacebookLoginPackage)
-                .addPackage(new GcmPackage())
-                .addPackage(new NotificationPackage(this))
-                .addPackage(new LinearGradientPackage())
-                .addPackage(new VectorIconsPackage())
-                .addPackage(new DesignSupportPackage(this))
-                .addPackage(new StatusBarPackage(this))
-                .addPackage(new WheelPackage())
-                .addPackage(new AppNativePackage(this))
-                .setUseDeveloperSupport(BuildConfig.DEBUG)
-                .setInitialLifecycleState(LifecycleState.RESUMED)
-                .build();
-
-        mReactRootView.startReactApplication(mReactInstanceManager, "ColorgyRNApp", null);
-
-        setContentView(mReactRootView);
+    protected String getMainComponentName() {
+        return "ColorgyRNApp";
     }
 
+    /**
+     * Returns whether dev mode should be enabled.
+     * This enables e.g. the dev menu.
+     */
     @Override
-    public boolean onKeyUp(int keyCode, KeyEvent event) {
-        if (keyCode == KeyEvent.KEYCODE_MENU && mReactInstanceManager != null) {
-            mReactInstanceManager.showDevOptionsDialog();
-            return true;
-        }
-        return super.onKeyUp(keyCode, event);
+    protected boolean getUseDeveloperSupport() {
+        return BuildConfig.DEBUG;
     }
 
+   /**
+   * A list of packages used by the app. If the app uses additional views
+   * or modules besides the default ones, add more packages here.
+   */
     @Override
-    public void onBackPressed() {
-      if (mReactInstanceManager != null) {
-        mReactInstanceManager.onBackPressed();
-      } else {
-        super.onBackPressed();
-      }
-    }
-
-    @Override
-    public void invokeDefaultOnBackPressed() {
-      super.onBackPressed();
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-
-        if (mReactInstanceManager != null) {
-            mReactInstanceManager.onPause();
-        }
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-
-        if (mReactInstanceManager != null) {
-            mReactInstanceManager.onResume(this, this);
-        }
-    }
-
-    @Override
-    public void onActivityResult(final int requestCode, final int resultCode, final Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        mFacebookLoginPackage.handleActivityResult(requestCode, resultCode, data);
+    protected List<ReactPackage> getPackages() {
+      return Arrays.<ReactPackage>asList(
+        new MainReactPackage(),
+        new RNDeviceInfo(),
+        new SQLitePluginPackage(this),
+        new GoogleAnalyticsPackage(this),
+        new FacebookLoginPackage(),
+        new GcmPackage(),
+        new NotificationPackage(this),
+        new LinearGradientPackage(),
+        new VectorIconsPackage(),
+        new DesignSupportPackage(this),
+        new StatusBarPackage(this),
+        new WheelPackage(),
+        new AppNativePackage(this));
     }
 }
